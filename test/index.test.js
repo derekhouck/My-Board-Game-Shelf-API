2475b1eab98419e4eca4efe739a009fda9765ea2
Derek Houck <derek@derekhouck.com>
2019-02-01 14:05:32 -0800

    Add reality check test

---
+++ test/index.test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const chai = require('chai');
+
+const expect = chai.expect;
+
+describe('Reality Check', () => {
+
+  it('true should be true', () => {
+    expect(true).to.be.true;
+  });
+
+  it('2 + 2 should equal 4', () => {
+    expect(2 + 2).to.equal(4);
+  });
+
+});
