abb777a2e89e44541122faf4db43309ab12e8b59
Derek Houck <derek@derekhouck.com>
2019-02-01 14:06:13 -0800

    Add test for environment

--- test/index.test.js
+++ test/index.test.js
@@ -15,3 +15,11 @@ describe('Reality Check', () => {
   });
 
 });
+
+describe('Environment', () => {
+
+  it('NODE_ENV should be "test"', () => {
+    expect(process.env.NODE_ENV).to.equal('test');
+  });
+
+});
