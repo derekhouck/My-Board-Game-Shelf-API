b8c83b74479eff3da72e85dc36098d17d6ed4916
Derek Houck <derek@derekhouck.com>
2019-02-06 11:44:57 -0800

    Finish seed-database.js

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -9,3 +9,29 @@ const Game = require('../models/game');
 const Tag = require('../models/tag');
 
 const { users, games, tags } = require('../db/data');
+
+console.log(`Connecting to mongodb at ${DATABASE_URL}`);
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useCreateIndex : true })
+  .then(() => {
+    console.info('Delete Data');
+    return mongoose.connection.db.dropDatabase();
+  })
+  .then(() => {
+    console.info('Seeding Database');
+    return Promise.all([
+      User.insertMany(users),
+      Game.insertMany(games),
+      Tag.insertMany(tags),
+      User.createIndexes(),
+      Tag.createIndexes()
+    ]);
+  })
+  .then(results => {
+    console.log('Inserted', results);
+    console.info('Disconnecting');
+    return mongoose.disconnect();
+  })
+  .catch(err => {
+    console.error(err);
+    return mongoose.disconnect();
+  });
